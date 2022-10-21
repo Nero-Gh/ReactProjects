07b4eb097640d891f6604eabfb1756ddb1e02527
Bismark Kadogbe <neroaboagye@gmail.com>
2022-10-21 02:14:07 +0000

    working with props on modal

---
+++ EmployeeProject/src/App.jsx
@@ -0,0 +1,81 @@
+import Employee from "./components/Employee";
+import "./App.css";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+function App() {
+  const [role, setRole] = useState("Software Engineer");
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      name: "John Doe",
+      role: "CEO",
+      image:
+        "https://images.pexels.com/photos/6758039/pexels-photo-6758039.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 2,
+      name: "Bismark Kadogbe",
+      role: "Software Engineer",
+      image:
+        "https://images.pexels.com/photos/6758039/pexels-photo-6758039.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 3,
+      name: "Nero Ben",
+      role: "Data Miner",
+      image:
+        "https://images.pexels.com/photos/14034247/pexels-photo-14034247.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    },
+    {
+      id: 4,
+      name: "Cynthia Ama",
+      role: "Labtician",
+      image:
+        "https://images.pexels.com/photos/6758039/pexels-photo-6758039.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 5,
+      name: "Kofi Baah",
+      role: "CEO",
+      image:
+        "https://images.pexels.com/photos/14034247/pexels-photo-14034247.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    },
+    {
+      id: 6,
+      name: "Felcia Aboagye",
+      role: "Entreprenure",
+      image:
+        "https://images.pexels.com/photos/6758039/pexels-photo-6758039.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+  ]);
+
+  const updateEmployee = (id, newName, newRole) => {
+    const updatedEmployee = employees.map((employee) => {
+      if (id == employee.id) {
+        return { ...employee, name: newName, role: newRole };
+      }
+      return employee;
+    });
+    setEmployees(updatedEmployee);
+  };
+
+  return (
+    <div className="App flex gap-2 flex-wrap w-7/12 mx-auto  items-center mt-10">
+      {employees.map((employee) => {
+        return (
+          <Employee
+            key={employee.id}
+            id={employee.id}
+            name={employee.name}
+            role={employee.role}
+            image={employee.image}
+            updateEmployee={updateEmployee}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
---
+++ EmployeeProject/src/components/Employee.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import EditEmployee from "./EditEmpolyee";
+
+function Employee(props) {
+  return (
+    <div className="py-8 px-8 max-w-sm mx-auto w-3/5 bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <img
+        className="block mx-auto object-cover h-24 w-24 rounded-full sm:mx-0 sm:shrink-0"
+        src={props.image}
+        alt="Woman's Face"
+      />
+      <div className="text-center space-y-2 sm:text-left">
+        <div className="space-y-0.5">
+          <p className="text-lg text-black font-semibold">{props.name}</p>
+          <p className="text-slate-500 font-medium">{props.role}</p>
+        </div>
+
+        <EditEmployee
+          id={props.id}
+          name={props.name}
+          role={props.role}
+          updateEmployee={props.updateEmployee}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Employee;
